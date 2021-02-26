51304d972cf01e27a8787e69bcceeeb7b0a2fd56
Rounak Hasan <hasan.rounak@gmail.com>
2021-02-26 16:27:53 +0530

    fluent validation implemented

---
+++ Validator/LoginRequestValdator.cs
@@ -0,0 +1,27 @@
+﻿using AuthenticationAndAutorization.Model;
+using FluentValidation;
+
+namespace AuthenticationAndAutorization.Validator
+{
+    public class LoginRequestValdator: AbstractValidator<LoginRequest>
+    {
+        public LoginRequestValdator()
+        {
+            this.RuleFor(x => x.UserName)
+            .NotEmpty()
+            .WithMessage("UserName can't be empty")
+            .MaximumLength(25)
+            .WithMessage("UserName should't have more than 25 Character")
+            .MinimumLength(3)
+            .WithMessage("UserName should have atleast  3 Character");
+
+            this.RuleFor(x => x.Password)
+           .NotEmpty()
+           .WithMessage("Password can't be empty")
+           .MaximumLength(25)
+           .WithMessage("Password should't have more than 25 Character")
+           .MinimumLength(8)
+           .WithMessage("Password should have atleast  8 Character");
+        }
+    }
+}
